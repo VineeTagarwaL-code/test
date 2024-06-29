d912c9a527010f8822988d77c5021d0553a48174
VineeTagarwaL-code <vineetagarwal.now@gmail.com>
2024-06-29 22:52:36 +0530

    fix

--- controllers/Ticketmaster.js
+++ controllers/Ticketmaster.js
@@ -1,40 +1,39 @@
-import axios from "axios";
-
+import { fetch } from "node-tls-client";
 const getTicketMasterPrice = async (eventId) => {
   try {
-    let config = {
-      method: "get",
-      maxBodyLength: Infinity,
-      url: `https://services.ticketmaster.com/api/ismds/event/${eventId}/quickpicks?mode=primary:ppsectionrow+resale:ga_areas+platinum:all&includeResale=true&embed=offer&apikey=b462oi7fic6pehcdkzony5bxhe&apisecret=pquzpfrfz7zd2ylvtz3w5dtyse&sort=totalprice`,
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:127.0) Gecko/20100101 Firefox/127.0",
-        Accept: "*/*",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate, br, zstd",
-        "TMPS-Correlation-Id": "47a5f4d7-d282-419c-b981-b85799317586",
-        Connection: "keep-alive",
-        Referer: "https://www.ticketmaster.com/",
-        Priority: "u=1",
-      },
-    };
     let minPrice = Infinity;
-    await axios.request(config).then((response) => {
-      const res = response.data;
+
+    await fetch(
+      `https://services.ticketmaster.com/api/ismds/event/${eventId}/quickpicks?mode=primary:ppsectionrow+resale:ga_areas+platinum:all&includeResale=true&embed=offer&apikey=b462oi7fic6pehcdkzony5bxhe&apisecret=pquzpfrfz7zd2ylvtz3w5dtyse&sort=totalprice`,
+      {
+        headers: {
+          "user-agent":
+            "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:127.0) Gecko/20100101 Firefox/127.0",
+          accept: "*/*",
+          "accept-language": "en-US,en;q=0.5",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "TMPS-Correlation-Id": "47a5f4d7-d282-419c-b981-b85799317586",
+          connection: "keep-alive",
+          referer: "https://www.ticketmaster.com/",
+          priority: "u=1",
+        },
+      }
+    ).then(async (response) => {
+      let res = await response.json();
       const listings = res?._embedded?.offer;
 
       if (!listings) return;
+
       for (const listing of listings) {
         if (listing.totalPrice < minPrice) {
           minPrice = listing.totalPrice;
         }
       }
     });
-
     return minPrice;
   } catch (err) {
     return null;
   }
 };
 
-export { getTicketMasterPrice };
+getTicketMasterPrice("3000608CCDD928C7").then((res) => console.log(res));
