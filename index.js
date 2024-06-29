8230f8201b764647f3ade78b919272fac4ff1062
VineeTagarwaL-code <vineetagarwal.now@gmail.com>
2024-06-29 22:53:04 +0530

    fix

--- index.js
+++ index.js
@@ -58,6 +58,6 @@ app.get("/vividSeatsPrice", async (req, res) => {
     console.log(err);
   }
 });
-app.listen(3001, () => {
+app.listen(80, () => {
   console.log("Server is running on port 3001");
 });
